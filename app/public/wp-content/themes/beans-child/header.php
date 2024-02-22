56039786e2c4ae7a7537059fe94c5c8abf950430
Romain <romain.turlotte@icloud.com>
2024-02-22 12:09:05 +0100

    #

--- app/public/wp-content/themes/beans-child/header.php
+++ app/public/wp-content/themes/beans-child/header.php
@@ -10,6 +10,6 @@
 <body <?php body_class(); ?>>
     <header class="header">
     <a href="<?php echo home_url( '/' ); ?>">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/Logo.png" alt="Logo">
     </a>  
     <?php wp_body_open(); ?>
