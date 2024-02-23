200747fd6949f2c7d16f447dc7d1b03661380eee
Romain <romain.turlotte@icloud.com>
2024-02-23 17:02:07 +0100

    Ajout de fichier php

--- app/public/wp-content/themes/beans-child/header.php
+++ app/public/wp-content/themes/beans-child/header.php
@@ -16,4 +16,8 @@
     <a href="<?php echo home_url('/'); ?>">
       <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
     </a>
+    <p>energy drink</p>
+    <nav>
+
+    </nav>
   </header>
